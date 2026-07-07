a2ce5864f8cbee5ffc1a6979590abf2959abc758
יעקב <yakov@localhost>
2026-07-07 21:38:04 +0300

    שינוי שם הריפו ל-daily-pdf-reader

--- web-app-config.js
+++ web-app-config.js
@@ -1,3 +1,3 @@
 (() => {
-  globalThis.DAILY_PDF_READER_WEB_APP_URL = "https://blueskydev2026.github.io/pdf-reader-extenuations/";
+  globalThis.DAILY_PDF_READER_WEB_APP_URL = "https://blueskydev2026.github.io/daily-pdf-reader/";
 })();
