77f96bad1ecddd100e27a7c4eca743b9a99d4fe6
yakov <yakov2413@gmail.com>
2026-07-08 15:16:36 +0300

    Remove unused context menu permission

--- extension/background.js
+++ extension/background.js
@@ -4,22 +4,8 @@ const PACKAGED_READER_URL = chrome.runtime.getURL("index.html");
 const WEB_READER_URL = normalizeWebAppUrl(globalThis.DAILY_PDF_READER_WEB_APP_URL);
 const READER_URL = WEB_READER_URL || PACKAGED_READER_URL;
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "open-reader",
-    title: "פתח את קורא PDF יומי",
-    contexts: ["action", "page", "selection", "link"]
-  });
-});
-
 chrome.action.onClicked.addListener(() => {
   openReader();
-});
-
-chrome.contextMenus.onClicked.addListener((info) => {
-  if (info.menuItemId === "open-reader") {
-    openReader();
-  }
 });
 
 function openReader() {
